d129f017a30fd640c8a1f8b6d6e53e83227d9eb0
alemax1 <maximov.270502@mail.ru>
2022-08-07 01:44:40 +0300

    test-project-women
    test-project-women

---
+++ coolsite/coolsite/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+
+from coolsite import settings
+from women.views import *
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
+    path('', include('women.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
